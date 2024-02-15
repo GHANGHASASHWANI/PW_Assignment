168264b4cf2cd26135f1c708a05ed05398086f47
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2024-02-15 18:47:16 +0530

    First_Assignment

---
+++ Basics/que3.cpp
@@ -0,0 +1,13 @@
+// Q3.How much space does the following data types take ?
+// ->int = 4
+// ->bool = 1
+// ->float = 4
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << sizeof(int) << endl;
+    cout << sizeof(bool) << endl;
+    cout << sizeof(float);
+}
---
+++ Basics/que5.cpp
@@ -0,0 +1,9 @@
+// Write a program to find the circumference of a circle with radius 10 in C++.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int radius=10;
+    float circumference=2*(3.14)*10;
+    cout<<circumference;
+}
